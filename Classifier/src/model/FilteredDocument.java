89af180ede680806d3ebf660573e7cc1862a895a
s1702297 <a.w.pool@student.utwente.nl>
2017-01-19 16:54:33 +0100

    Updates

--- Classifier/src/model/FilteredDocument.java
+++ Classifier/src/model/FilteredDocument.java
@@ -6,4 +6,11 @@ public class FilteredDocument {
 	
 	private ArrayList<String> wordsInDocument = new ArrayList<String>();
 
+	public ArrayList<String> getWords(){
+		return wordsInDocument;
+	}
+	
+	public void setWords(ArrayList<String> words) {
+		wordsInDocument = words;
+	}
 }
